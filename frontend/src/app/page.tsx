4b565b223850bae67dcb073ddfdd98185fc08183
webtrovert0x <philipogunwole261@gmail.com>
2026-08-21 14:13:37 +0100

    Fix iOS click bug

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -101,7 +101,7 @@ const HomeInbox = ({ playerName, setPlayerName, roomCode, setRoomCode, onHost, o
       {error && <div className="error-text">{error}</div>}
 
       {/* Host Game Button */}
-      <div className="inbox-item" onClick={onHost}>
+      <div className="inbox-item" onClick={onHost} role="button" tabIndex={0}>
         <div className="avatar host-avatar">
            <svg viewBox="0 0 24 24"><path fill="white" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z"/></svg>
         </div>
